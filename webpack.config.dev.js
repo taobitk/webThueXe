c740a95445c8a1a7055a2b698dfd94f600f335dd
taobitk <8hungvip8@gmail.com>
2025-12-24 20:46:52 +0700

    sec commit

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyPlugin = require('copy-webpack-plugin'); // <--- 1. BẮT BUỘC PHẢI CÓ DÒNG NÀY
 
 module.exports = merge(common, {
   mode: 'development',
@@ -10,4 +11,26 @@ module.exports = merge(common, {
     open: true,
     static: ['./'],
   },
+
+  // 2. BẮT BUỘC PHẢI ĐẶT TRONG 'plugins: []'
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'img', to: 'img' },
+        { from: 'css', to: 'css' },
+        { from: 'js/vendor', to: 'js/vendor' },
+        { from: 'icon.svg', to: 'icon.svg' },
+        { from: 'favicon.ico', to: 'favicon.ico' },
+        { from: 'robots.txt', to: 'robots.txt' },
+        { from: 'icon.png', to: 'icon.png' },
+        { from: '404.html', to: '404.html' },
+        { from: 'site.webmanifest', to: 'site.webmanifest' },
+
+        // --- CÁC DÒNG BẠN THÊM ---
+        { from: 'anh.jpg', to: 'anh.jpg' },
+        { from: 'index2.html', to: 'index2.html' }
+        // -------------------------
+      ],
+    }),
+  ],
 });
